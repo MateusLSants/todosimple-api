e292f33957eba6069670d3e118354a3e42b53ccc
MateusLSants <mateusleandro23@gmail.com>
2023-04-08 15:44:33 -0300

    Spring boot project

---
+++ src/test/java/br/com/dev/todosimple/todosimple/TodosimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dev.todosimple.todosimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
