e69f9df129b78adf96fd8c26bbe1dea966a50571
MateusLSants <mateusleandro23@gmail.com>
2023-04-08 16:35:28 -0300

    created user and AbstractEntity models

--- src/main/java/br/com/dev/todosimple/todosimple/model/AbstractEntity.java
+++ src/main/java/br/com/dev/todosimple/todosimple/model/AbstractEntity.java
@@ -1,2 +1,35 @@
-package br.com.dev.todosimple.todosimple.model;public class AbstractEntity {
+package br.com.dev.todosimple.todosimple.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@MappedSuperclass
+public class AbstractEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractEntity that = (AbstractEntity) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- src/main/java/br/com/dev/todosimple/todosimple/model/User.java
+++ src/main/java/br/com/dev/todosimple/todosimple/model/User.java
@@ -1,2 +1,63 @@
-package br.com.dev.todosimple.todosimple.model;public class User {
+package br.com.dev.todosimple.todosimple.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = User.TABLE_NAME)
+public class User extends AbstractEntity {
+    public static final String TABLE_NAME = "user";
+
+    @Column(name = "name", length = 100, nullable = false, unique = false)
+    @NotNull
+    @NotEmpty
+    @Size(min = 2, max = 100)
+    private String name;
+    @Column(name = "password", length = 60, nullable = false)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotNull
+    @NotEmpty
+    @Size(min = 8, max = 60)
+    private String password;
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(name, user.name) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, password);
+    }
 }
