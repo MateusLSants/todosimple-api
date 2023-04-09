325ee166299f3ad67126f4bb9cc851c69e89e238
MateusLSants <mateusleandro23@gmail.com>
2023-04-09 10:01:05 -0300

    refactor UserService.java

--- src/main/java/br/com/dev/todosimple/todosimple/service/UserService.java
+++ src/main/java/br/com/dev/todosimple/todosimple/service/UserService.java
@@ -8,10 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
 @Service
 public class UserService {
-
     @Autowired
     private UserRepository userRepository;
     @Autowired
