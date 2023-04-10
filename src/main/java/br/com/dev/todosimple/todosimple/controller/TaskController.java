e7d338ae0ac6065c3c1446d7a8f990612b8d8097
MateusLSants <mateusleandro23@gmail.com>
2023-04-10 11:08:49 -0300

    create TaskController.java

---
+++ src/main/java/br/com/dev/todosimple/todosimple/controller/TaskController.java
@@ -0,0 +1,47 @@
+package br.com.dev.todosimple.todosimple.controller;
+
+import br.com.dev.todosimple.todosimple.model.Task;
+import br.com.dev.todosimple.todosimple.service.TaskService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/task")
+@Validated
+public class TaskController {
+    @Autowired
+    TaskService taskService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> findById(@PathVariable Long id) {
+        Task task = taskService.findById(id);
+        return ResponseEntity.ok(task);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Void> save(@RequestBody Task task) {
+        this.taskService.save(task);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(task.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@Valid @RequestBody Task task, @PathVariable Long id) {
+        task.setId(id);
+        this.taskService.update(task);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable long id) {
+        this.taskService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
