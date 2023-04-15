ea36028610bc056825957189b80245aca7e898fe
MateusLSants <mateusleandro23@gmail.com>
2023-04-15 12:10:16 -0300

    add lombok

--- src/main/java/br/com/dev/todosimple/todosimple/model/AbstractEntity.java
+++ src/main/java/br/com/dev/todosimple/todosimple/model/AbstractEntity.java
@@ -1,35 +1,15 @@
 package br.com.dev.todosimple.todosimple.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 @MappedSuperclass
+@Data
 public class AbstractEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", unique = true)
     private long id;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        AbstractEntity that = (AbstractEntity) o;
-        return id == that.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
