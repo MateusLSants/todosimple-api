096133da289317eb104a1d425f55958daeb18fc7
MateusLSants <mateusleandro23@gmail.com>
2023-04-15 12:06:27 -0300

    add lombok

--- src/main/java/br/com/dev/todosimple/todosimple/model/Task.java
+++ src/main/java/br/com/dev/todosimple/todosimple/model/Task.java
@@ -4,49 +4,22 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.*;
 
-import java.util.Objects;
-
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = Task.TABLE_NAME)
+@Data
 public class Task extends AbstractEntity {
     public static final String TABLE_NAME = "task";
+
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false, updatable = false)
     private User user;
+
     @Column(name = "description", length = 255, nullable = false)
     @NotNull
     @NotEmpty
     @Size(min = 1, max = 255)
     private String description;
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        Task task = (Task) o;
-        return Objects.equals(user, task.user) && Objects.equals(description, task.description);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), user, description);
-    }
 }
--- src/main/java/br/com/dev/todosimple/todosimple/model/User.java
+++ src/main/java/br/com/dev/todosimple/todosimple/model/User.java
@@ -1,6 +1,5 @@
 package br.com.dev.todosimple.todosimple.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -9,63 +8,32 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = User.TABLE_NAME)
+@Data
 public class User extends AbstractEntity {
     public static final String TABLE_NAME = "user";
+
     @Column(name = "name", length = 100, nullable = false, unique = false)
     @NotNull
     @NotEmpty
     @Size(min = 2, max = 100)
     private String name;
+
     @Column(name = "password", length = 60, nullable = false)
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @NotNull
     @NotEmpty
     @Size(min = 8, max = 60)
     private String password;
+
     @OneToMany(mappedBy = "user")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private List<Task> tasks = new ArrayList<Task>();
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    @JsonIgnore
-    public List<Task> getTasks() {
-        return tasks;
-    }
-
-    public void setTasks(List<Task> tasks) {
-        this.tasks = tasks;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(name, user.name) && Objects.equals(password, user.password);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, password);
-    }
 }
